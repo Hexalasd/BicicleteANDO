6622bda39270573c31008e46993004826ffe09a1
hexal <hexala1234@gmail.com>
2025-05-29 23:51:49 -0300

    me voy a pegar un tiro

--- src/logica/Principal.java
+++ src/logica/Principal.java
@@ -2,3 +2,11 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+package logica;
+
+import java.util.ArrayList;
+
+public class Principal {
+     public static ArrayList<Bicicleta> listaBicicletas = new ArrayList<>();
+     
+}
