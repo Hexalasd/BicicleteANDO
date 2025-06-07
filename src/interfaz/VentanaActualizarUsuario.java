4d606e85071158c290447a333432153cd30c6b76
57815971 <57815971@T140J2312027023>
2025-06-06 23:40:03 -0300

    ricas cosas

--- src/interfaz/VentanaActualizarUsuario.java
+++ src/interfaz/VentanaActualizarUsuario.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
 package interfaz;
+import logica.ArrayUsuarios;
 import logica.Usuario;
 /**
  *
@@ -15,6 +16,7 @@ public class VentanaActualizarUsuario extends javax.swing.JFrame {
      */
     public VentanaActualizarUsuario() {
         initComponents();
+        cargarCombo();
     }
 
     /**
@@ -26,7 +28,7 @@ public class VentanaActualizarUsuario extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jComboBox1 = new javax.swing.JComboBox<>();
+        comboUsuarios = new javax.swing.JComboBox<>();
         jLabel1 = new javax.swing.JLabel();
         btnViaje = new javax.swing.JButton();
         btnRevisarBici = new javax.swing.JButton();
@@ -41,10 +43,10 @@ public class VentanaActualizarUsuario extends javax.swing.JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
-        jComboBox1.addActionListener(new java.awt.event.ActionListener() {
+        comboUsuarios.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+        comboUsuarios.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jComboBox1ActionPerformed(evt);
+                comboUsuariosActionPerformed(evt);
             }
         });
 
@@ -127,7 +129,7 @@ public class VentanaActualizarUsuario extends javax.swing.JFrame {
                         .addGap(73, 73, 73)
                         .addComponent(jLabel1)
                         .addGap(33, 33, 33)
-                        .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addComponent(comboUsuarios, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(layout.createSequentialGroup()
                         .addGap(19, 19, 19)
                         .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 346, javax.swing.GroupLayout.PREFERRED_SIZE)))
@@ -143,7 +145,7 @@ public class VentanaActualizarUsuario extends javax.swing.JFrame {
                 .addGap(19, 19, 19)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel1)
-                    .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(comboUsuarios, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(37, 37, 37)
@@ -165,9 +167,9 @@ public class VentanaActualizarUsuario extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jComboBox1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jComboBox1ActionPerformed
+    private void comboUsuariosActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_comboUsuariosActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jComboBox1ActionPerformed
+    }//GEN-LAST:event_comboUsuariosActionPerformed
 
     private void btnViajeActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnViajeActionPerformed
         double km;
@@ -261,10 +263,16 @@ public class VentanaActualizarUsuario extends javax.swing.JFrame {
     private javax.swing.JButton btnMostrarDatos;
     private javax.swing.JButton btnRevisarBici;
     private javax.swing.JButton btnViaje;
+    private javax.swing.JComboBox<String> comboUsuarios;
     private javax.swing.JButton jButton11;
-    private javax.swing.JComboBox<String> jComboBox1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTextArea txtArea;
     // End of variables declaration//GEN-END:variables
+
+    private void cargarCombo() {
+        for (Usuario u : ArrayUsuarios.getUsuarios()) {
+        comboUsuarios.addItem(u);
+        }
+    }
 }
